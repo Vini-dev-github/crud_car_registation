2a088bb72d1d3fdebeefb0f5c78300b28513b2a6
Vini <viniciuslima.profissional@gmail.com>
2024-05-20 15:11:08 -0300

    create index and form

--- app_registration/views.py
+++ app_registration/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('Hell-o world')
+    return render(request, 'index.html')
+
+def form(request):
+    return render(request, 'form.html')
--- project_registration/urls.py
+++ project_registration/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from app_registration.views import home
+from app_registration.views import home, form
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home)
+    path('', home),
+    path('form/', form)
 ]
