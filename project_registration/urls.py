733ca227292cb59dd3605e3afe1034f78e692a90
Vini <viniciuslima.profissional@gmail.com>
2024-05-20 21:55:57 -0300

    edit and update

--- project_registration/urls.py
+++ project_registration/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from app_registration.views import home, form, create, view
+from app_registration.views import home, form, create, view, edit, update
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('form/', form, name='form'),
     path('create/', create, name='create'),
-    path('view/<int:pk>', view, name='view')
+    path('view/<int:pk>/', view, name='view'),
+    path('edit/<int:pk>/', edit, name='edit'),
+    path('update/<int:pk>/', update, name='update'),
 ]
